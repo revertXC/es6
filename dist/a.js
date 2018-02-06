96acd8a3ccc1de699f7ac95ea1d9aa5b2394d1fa
xcshuai <1376477341@qq.com>
2018-02-06 20:50:32 +0800

    新增二个JS

--- dist/a.js
+++ dist/a.js
@@ -1,6 +1,7 @@
 /*Node.js 内置对象 fs 模块 文件系统模块，负责读写文件*/
 var fs = require("fs");
-
+// 设置编码为 utf8。
+// readerStream.setEncoding('UTF8');
 let txtUrl = "C:\\Users\\Administrator\\Desktop\\input.txt";
 
 /* 同步读取数据*/
